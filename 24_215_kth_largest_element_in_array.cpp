27281142c254c3fe1eb28e788af3a664e72a2149
KrupalTank <krupaltank777@gmail.com>
2025-07-31 08:36:08 +0530

    Add files via upload

---
+++ 24_215_kth_largest_element_in_array.cpp
@@ -0,0 +1,18 @@
+#include<vector>
+#include<queue>
+using namespace std;
+class Solution {
+public:
+    int findKthLargest(std::vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> min_heap(nums.begin(), nums.begin() + k);
+        
+        for (int i = k; i < nums.size(); i++) {
+            if (nums[i] > min_heap.top()) {
+                min_heap.pop();
+                min_heap.push(nums[i]);
+            }
+        }
+        
+        return min_heap.top();
+    }
+};

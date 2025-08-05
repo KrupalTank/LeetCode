b41e272dc72fc96765d5ed97c6327962c80f806e
KrupalTank <krupaltank777@gmail.com>
2025-08-05 08:40:54 +0530

    Add files via upload

---
+++ 29_94_binary_tree_inorder_traversal.cpp
@@ -0,0 +1,34 @@
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+ 
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int>v;
+        inorder(v, root);
+        return v;
+    }
+
+private:
+    void inorder(vector<int>&v, TreeNode *root)
+    {
+        if(root == NULL)
+        {
+            return;
+        }
+        inorder(v, root->left);
+        v.push_back(root->val);
+        inorder(v, root->right);
+    }
+};
